027c43679e4b6af1cc324284df359eedafb043f6
Patrice Thivierge F <pthivierge@gmail.com>
2015-12-04 19:11:43 +0100

    Added new event frame utility
    Current state is still work in progress

---
+++ Core/3-Utility/AFEventFramesUtil/AFEventFramesUtil.cs
@@ -0,0 +1,106 @@
+﻿#region Copyright
+//  Copyright 2015 OSIsoft, LLC
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+#endregion
+using System;
+using System.ComponentModel;
+using CommandLine;
+using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.PI;
+using Clues.Library;
+using Clues.Library.DataHandling;
+
+namespace Clues
+{
+
+    [Description("Utility to Create, Edit, Delete event frames from a csv file.")]
+    [AdditionalDescription("You can run commands to test if the data parsing is correct.  To make the write occur against the server, you need to specify the --EnableWrite option.")]
+    [UsageExample("AFEventFramesUtil -s SRV -d afDatabase -c -f data.csv")]
+    public class AFEventFramesUtil : AppletBase
+    {
+        //Command line Options
+        [Option('s', "AFServer", HelpText = "AF Server to connect to", Required = false)]
+        public string Server { get; set; }
+
+        [Option('d', "database", HelpText = "Name of the AF database use", Required = false)]
+        public string Database { get; set; }
+
+        [Option('c', "create", HelpText = "Create new event frames from the csv file passed", Required = false, MutuallyExclusiveSet = "CreateEditDeleteTest")]
+        public bool Create { get; set; }
+
+        [Option('e', "edit", HelpText = "Edit event frames", Required = false, MutuallyExclusiveSet = "CreateEditDeleteTest")]
+        public bool Edit { get; set; }
+
+        [Option('d', "delete", HelpText = "Name of the AF database use", Required = false, MutuallyExclusiveSet = "CreateEditDeleteTest")]
+        public bool Delete { get; set; }
+
+        [Option('t', "test", HelpText = "Name of the AF database use", Required = false, MutuallyExclusiveSet = "CreateEditDeleteTest")]
+        public bool Test { get; set; }
+
+        [Option('f', "file", HelpText = "Name of the AF database use", Required = true)]
+        public string File { get; set; }
+
+        [Option("EnableWrite", HelpText = "To enable the execution of write commands on the AF Server.", Required = false)]
+        public bool EnableWrite { get; set; }
+
+        private void CheckOption()
+        {
+            if ((Create || Edit || Delete) && EnableWrite)
+            {
+                if (string.IsNullOrEmpty(Database) || string.IsNullOrEmpty(Server))
+                {
+                    throw new InvalidParameterException("The --Database and the --Server options needs to be supplied when Write is enabled.");
+                }
+            }
+        }
+
+        public override void Run()
+        {
+
+            try
+            {
+
+
+                CheckOption();
+
+                if (Test)
+                {
+                    var loader=new FileLoader();
+                    loader.LoadFile(File);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+
+            //// connects to AF
+            //AFDatabase afDatabase;
+            //var afConnectionHelper = AfConnectionHelper.ConnectAndGetDatabase(Server, Database, out afDatabase);
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Core/Library/DataHandling/FileLoader.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using log4net.Repository.Hierarchy;
+
+namespace Clues.Library.DataHandling
+{
+    public class FileLoader
+    {
+        private readonly ILog Logger = LogManager.GetLogger(typeof(FileLoader).Name);
+
+        private readonly Dictionary<string, ColumnDefinition> referenceCols = null;
+
+        private DataTable table = new DataTable();
+
+        public FileLoader()
+        {
+            referenceCols = KnownHeaders();
+        }
+
+        //public DataTable GetData(Type classType, string filePath)
+        //{
+        //    var engine = new FileHelperEngine<>
+
+        //    var dt = engine.ReadFileAsDT(filePath);
+
+        //    return dt;
+        //}
+
+
+
+
+        public void LoadFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("The supplied file does not exist.");
+
+            var lines = File.ReadLines(filePath);
+
+            // Check first line, if it contains knows headers.
+            bool first = true;
+            foreach (var line in lines)
+            {
+                if (first)
+                {
+                    CheckHeader(line);
+                    first = false;
+                }
+
+                else
+                {
+                    LoadLine(line);
+                }
+            }
+
+            // debug
+            PrintDataTable(table);
+
+
+        }
+
+        private void LoadLine(string line)
+        {
+            List<string> columnData = GetFields(line);
+
+
+            var newRow = table.NewRow();
+            foreach (DataColumn column in table.Columns)
+            {
+                // get the position in the dataset that correspond to the column
+                var pos = referenceCols[column.ColumnName].Position;
+
+                // update the "cell" with the data
+                newRow[column.ColumnName] = columnData[pos];
+            }
+
+            table.Rows.Add(newRow);
+        }
+
+        private void CheckHeader(string line)
+        {
+            List<string> columnHeaders = GetFields(line);
+
+            int position = 0;
+            foreach (var columnName in columnHeaders)
+            {
+                if (referenceCols.ContainsKey(columnName) && referenceCols[columnName].Count == 0)
+                {
+                    // maintains 
+                    referenceCols[columnName].Count = 1;
+                    referenceCols[columnName].Position = position;
+                    table.Columns.Add(columnName);
+                }
+
+                position++;
+            }
+
+            Logger.InfoFormat("The file headers was checked and {0} headers have been loaded out of {1} the file contains.", table.Columns.Count, columnHeaders.Count);
+
+        }
+
+        private static List<string> GetFields(string line)
+        {
+            return line.Split('\t').ToList().Select(s => s.Trim().ToLower()).ToList();
+        }
+
+        private Dictionary<string, ColumnDefinition> KnownHeaders()
+        {
+            var knowsHeaders =
+                "Selected(x)	Parent	Name	ObjectType	NewName	UniqueID	NewParent	Description	ReferenceType	Template	PrimaryReferencedElement	DefaultAttribute	Categories	IsLocked	AreValuesCaptured	StartTime	EndTime	ModifyDate	SecurityString	ExtendedPropertyType	ExtendedPropertyValue	AttributeIsHidden	AttributeIsConfigurationItem	AttributeIsExcluded	AttributeDefaultUOM	AttributeType	AttributeTypeQualifier	AttributeValue	AttributeDataReference	AttributeConfigString"
+                    .Split('\t').ToList().Select(s => s.Trim().ToLower()).ToList();
+
+            var dictionary = new Dictionary<string, ColumnDefinition>();
+
+            knowsHeaders.ForEach(h =>
+            {
+                if (!dictionary.ContainsKey(h))
+                    dictionary.Add(h, new ColumnDefinition());
+            });
+
+            return dictionary;
+
+        }
+
+
+        public void PrintDataTable(DataTable table)
+        {
+
+            foreach (DataColumn dataColumn in table.Columns)
+            {
+                Console.Write(dataColumn.ColumnName + '\t');
+            }
+
+            Console.Write('\n');
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var data in row.ItemArray)
+                {
+                    Console.Write(data.ToString() + '\t');
+                }
+                Console.Write('\n');
+            }
+        }
+
+
+        public class ColumnDefinition
+        {
+            public int Count { get; set; }
+            public int Position { get; set; }
+        }
+    }
+}
